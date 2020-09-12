0ce9ddd1d1c742ac3e1507159a5763f4a1f17e68
Viral Lalakia <viral.lalakia@gmail.com>
2020-09-12 18:32:19 +0530

    Add solution for Yet Another Two Integers Problem problem on Codeforces

---
+++ code/java/com/virallalakia/codeforces/YetAnotherTwoIntegersProblem.java
@@ -0,0 +1,48 @@
+/*
+ ******************************************************************************
+ * MIT License                                                                *
+ * Copyright (c) 2020 - Present Viral Lalakia                                 *
+ ******************************************************************************
+ */
+
+package com.virallalakia.codeforces;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+/**
+ * This is a solution for Yet Another Two Integers Problem problem of Codeforces.
+ * This is verified on https://codeforces.com. Package definition needs to be removed before submission.
+ * 
+ * @author Viral Lalakia 
+ */
+public class YetAnotherTwoIntegersProblem {
+
+  private static final Scanner SYS_IN =
+      new Scanner(new BufferedReader(new InputStreamReader(System.in)));
+
+  /**
+   * Main method for solution. Calls to evaluate one of more test cases.
+   */
+  public static void main(String[] args) {
+    final int t = SYS_IN.nextInt(); // total test cases
+    for (int ti = 0; ti < t; ti++) {
+      evaluateCase();
+    }
+    SYS_IN.close();
+  }
+
+  /**
+   * Evaluate a test case. Handles I/O.
+   */
+  private static void evaluateCase() {
+    try {
+      int a = SYS_IN.nextInt();
+      int b = SYS_IN.nextInt();
+      SYS_IN.nextLine();
+
+      System.out.println((Math.abs(a - b) + 9) / 10);
+    } catch (Exception e) {}
+  }
+}

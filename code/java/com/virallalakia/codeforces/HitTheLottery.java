e454c8857202d1a56490785e9d4e9ed75147b7f2
Viral Lalakia <viral.lalakia@gmail.com>
2020-09-14 20:50:21 +0530

    Add solution for Hit the Lottery problem on Codeforces

---
+++ code/java/com/virallalakia/codeforces/HitTheLottery.java
@@ -0,0 +1,50 @@
+/*
+ ******************************************************************************
+ * MIT License                                                                *
+ * Copyright (c) 2020 - Present Viral Lalakia                                 *
+ ******************************************************************************
+ */
+
+package com.virallalakia.codeforces;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+/**
+ * This is a solution for Hit the Lottery problem of Codeforces.
+ * This is verified on https://codeforces.com. Package definition needs to be removed before submission.
+ * 
+ * @author Viral Lalakia 
+ */
+public class HitTheLottery {
+
+  private static final Scanner SYS_IN =
+      new Scanner(new BufferedReader(new InputStreamReader(System.in)));
+
+  /**
+   * Main method for solution. Calls to evaluate one or more test cases.
+   */
+  public static void main(String[] args) {
+    evaluateCase();
+    SYS_IN.close();
+  }
+
+  /**
+   * Evaluate a test case. Handles I/O.
+   */
+  private static void evaluateCase() {
+    try {
+      int n = SYS_IN.nextInt();
+      SYS_IN.nextLine();
+
+      int[] bills = {100, 20, 10, 5, 1};
+      int ans = 0;
+      for (int bill : bills) {
+        ans += n / bill;
+        n %= bill;
+      }
+      System.out.println(ans);
+    } catch (Exception e) {}
+  }
+}

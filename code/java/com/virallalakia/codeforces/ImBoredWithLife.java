014496acbe8953e43d7b14fa9f7e625c16c67078
Viral Lalakia <viral.lalakia@gmail.com>
2020-09-14 19:58:21 +0530

    Add solution for I'm bored with life problem on Codeforces

---
+++ code/java/com/virallalakia/codeforces/ImBoredWithLife.java
@@ -0,0 +1,49 @@
+/*
+ ******************************************************************************
+ * MIT License                                                                *
+ * Copyright (c) 2020 - Present Viral Lalakia                                 *
+ ******************************************************************************
+ */
+
+package com.virallalakia.codeforces;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+/**
+ * This is a solution for I'm bored with life problem of Codeforces.
+ * This is verified on https://codeforces.com. Package definition needs to be removed before submission.
+ * 
+ * @author Viral Lalakia 
+ */
+public class ImBoredWithLife {
+
+  private static final Scanner SYS_IN =
+      new Scanner(new BufferedReader(new InputStreamReader(System.in)));
+
+  /**
+   * Main method for solution. Calls to evaluate one or more test cases.
+   */
+  public static void main(String[] args) {
+    evaluateCase();
+    SYS_IN.close();
+  }
+
+  /**
+   * Evaluate a test case. Handles I/O.
+   */
+  private static void evaluateCase() {
+    try {
+      int a = SYS_IN.nextInt();
+      int b = SYS_IN.nextInt();
+      SYS_IN.nextLine();
+
+      int ans = 1;
+      for (int i = 2; i <= Math.min(a, b); i++) {
+        ans *= i;
+      }
+      System.out.println(ans);
+    } catch (Exception e) {}
+  }
+}

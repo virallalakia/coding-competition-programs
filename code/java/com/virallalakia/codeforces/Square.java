51d51a82b8d55de7f4e7f23b5791f662f4f96a1b
Viral Lalakia <viral.lalakia@gmail.com>
2020-09-14 22:05:52 +0530

    Add solution for Square problem on Codeforces

---
+++ code/java/com/virallalakia/codeforces/Square.java
@@ -0,0 +1,55 @@
+/*
+ ******************************************************************************
+ * MIT License                                                                *
+ * Copyright (c) 2020 - Present Viral Lalakia                                 *
+ ******************************************************************************
+ */
+
+package com.virallalakia.codeforces;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+/**
+ * This is a solution for Square? Problem problem of Codeforces.
+ * This is verified on https://codeforces.com. Package definition needs to be removed before submission.
+ * 
+ * @author Viral Lalakia 
+ */
+public class Square {
+
+  private static final Scanner SYS_IN =
+      new Scanner(new BufferedReader(new InputStreamReader(System.in)));
+
+  /**
+   * Main method for solution. Calls to evaluate one or more test cases.
+   */
+  public static void main(String[] args) {
+    final int t = SYS_IN.nextInt(); // total test cases
+    for (int ti = 0; ti < t; ti++) {
+      evaluateCase();
+    }
+    SYS_IN.close();
+  }
+
+  /**
+   * Evaluate a test case. Handles I/O.
+   */
+  private static void evaluateCase() {
+    try {
+      int a1 = SYS_IN.nextInt();
+      int b1 = SYS_IN.nextInt();
+      SYS_IN.nextLine();
+      int a2 = SYS_IN.nextInt();
+      int b2 = SYS_IN.nextInt();
+      SYS_IN.nextLine();
+
+      System.out.println(
+        (Math.max(a1, b1) == Math.max(a2, b2)
+          && Math.max(a1, b1) == Math.min(a1, b1) + Math.min(a2, b2))
+        ? "YES"
+        : "NO");
+    } catch (Exception e) {}
+  }
+}

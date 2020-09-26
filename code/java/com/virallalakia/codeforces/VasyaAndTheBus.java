006ad8cba4d7cabdf2a132c0da1870564d0ef73d
Viral Lalakia <viral.lalakia@gmail.com>
2020-09-26 20:49:46 +0530

    Add solution for Vasya and the Bus problem on Codeforces

---
+++ code/java/com/virallalakia/codeforces/VasyaAndTheBus.java
@@ -0,0 +1,49 @@
+/*
+ ******************************************************************************
+ * MIT License                                                                *
+ * Copyright (c) 2020 - Present Viral Lalakia                                 *
+ ******************************************************************************
+ */
+
+package com.virallalakia.codeforces;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+/**
+ * This is a solution for Vasya and the Bus problem of Codeforces.
+ * This is verified on https://codeforces.com. Package definition needs to be removed before submission.
+ * 
+ * @author Viral Lalakia 
+ */
+public class VasyaAndTheBus {
+
+  private static final Scanner SYS_IN =
+      new Scanner(new BufferedReader(new InputStreamReader(System.in)));
+
+  /**
+   * Main method for solution. Calls to evaluate one or more test cases.
+   */
+  public static void main(String[] args) {
+    evaluateCase();
+    SYS_IN.close();
+  }
+
+  /**
+   * Evaluate a test case. Handles I/O.
+   */
+  private static void evaluateCase() {
+    try {
+      int n = SYS_IN.nextInt();
+      int m = SYS_IN.nextInt();
+      SYS_IN.nextLine();
+
+      if (n == 0 && m > 0) {
+        System.out.println("Impossible");
+      } else {
+        System.out.println(((long)n + m - Math.min(n, m)) + " " + ((long)n + m - Math.min(1, m)));
+      }
+    } catch (Exception e) {}
+  }
+}
